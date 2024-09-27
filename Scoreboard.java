33af1bb9f0d5d0df6ce1273f65525633a87fbd0f
Gabriel-Ricci <gabrielr244@nycstudents.net>
2024-09-27 13:32:32 +0000

    skibidi

---
+++ Scoreboard.java
@@ -0,0 +1,23 @@
+public class Scoreboard
+    {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;
+        private String teamOne;
+        private String teamTwo;
+        private int teamOneScore;
+        private int teamTwoScore;
+        private String activeTeam;
+        public Scoreboard(String one, String two)
+        {
+            teamOne = one;
+            teamTwo = two;
+        }
+        public String getScore()
+        {
+            return null;
+        }
+    }
+        
+
+
